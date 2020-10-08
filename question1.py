e1dba87d3272174d45db417dec198db5430b5b4e
Luis Martinez <luis.martinez@daviestech.org>
2020-10-08 20:03:46 +0000

    finished

---
+++ question1.py
@@ -0,0 +1,4 @@
+number = int(input ("Enter a number: "))
+print(number+1)
+print(number+2)
+print(number+3)
---
+++ question2.py
@@ -0,0 +1,4 @@
+number1 = float(input ("Enter a number: "))
+number2= float(input ("Enter another number: "))
+number3= float(input ("Enter one last number: "))
+print(number1 + number2 + number3)
